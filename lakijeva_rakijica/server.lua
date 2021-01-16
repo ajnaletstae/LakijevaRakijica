b1a7e104fe9001e3bb0127fa5c1c2d9fc00f897e
Lakisha <68185793+Lazar-cyber@users.noreply.github.com>
2021-01-16 23:02:42 +0100

    Mali update
    Fix mali

--- lakijeva_rakijica/server.lua
+++ lakijeva_rakijica/server.lua
@@ -5,10 +5,10 @@ TriggerEvent('esx:getSharedObject', function(obj)
 	ESX = obj
 end)
 
-ESX.RegisterUsableItem('rakijica', function(baza)
-    local podbaza = baza
-	local xPlayer = ESX.GetPlayerFromId(podbaza)
+ESX.RegisterUsableItem('rakijica', function(source)
+    local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
 	xPlayer.removeInventoryItem('rakijica', 1)
-	TriggerClientEvent('lakijeva-rakija:onRakijica', baza)
+	TriggerClientEvent('lakijeva-rakija:onRakijica', _source)
 end)
 
